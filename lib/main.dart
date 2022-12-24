6cf8647d94d3d609f7d74c3e6b33991606e4eff0
hogelog <konbu.komuro@gmail.com>
2022-12-24 21:01:18 +0900

    Implement transcript by whisper.cpp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:cross_file/cross_file.dart';
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+const APPDIR = 'org.hogel.whispercppapp';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +37,10 @@ class HomeWidget extends StatefulWidget {
 class _HomeWidgetState extends State<HomeWidget> {
   XFile? _dropFile = null;
   bool _dragging = false;
+  bool _converting = false;
+  String _consoleText = "";
+
+  Directory? _appTempDir;
 
   @override
   Widget build(BuildContext context) {
@@ -72,19 +81,78 @@ class _HomeWidgetState extends State<HomeWidget> {
                 ),
               ),
             ),
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: SelectableText(
+                _consoleText,
+              ),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _dropFile == null ? null : _runRecognition,
+        onPressed: _runnable() ? _runRecognition : null,
         tooltip: 'Run recognition',
-        child: const Icon(Icons.play_arrow),
-        backgroundColor: _dropFile == null ? Colors.black38 : Colors.blue,
+        child: _converting ? const CircularProgressIndicator(color: Colors.blue) : const Icon(Icons.play_arrow),
+        backgroundColor: _runnable() ? Colors.blue : Colors.black38,
       ),
     );
   }
 
-  void _runRecognition() {
-    log(_dropFile!.path);
+  bool _runnable() => !_converting && _dropFile != null;
+
+  Future<Directory?> _setup() async {
+    Directory userTempDir = await getTemporaryDirectory();
+    _appTempDir = await Directory(path.join(userTempDir.path, APPDIR)).create();
+    return _appTempDir;
+  }
+
+  void _runRecognition() async {
+    await _setup();
+
+    setState(() {
+      _converting = true;
+      _consoleText = "";
+    });
+    try {
+      File wavfile = await _convertWavfile(_dropFile!.path);
+      await _transcript(wavfile);
+    } finally {
+      setState(() {
+        _dropFile = null;
+        _converting = false;
+      });
+    }
+  }
+
+  Future<File> _convertWavfile(String sourceFile) async {
+    File wavfile = File(path.join(_appTempDir!.path, "input.wav"));
+    if (wavfile.existsSync()) {
+      wavfile.deleteSync();
+    }
+    var args = ['-i', _dropFile!.path, '-ar', '16000', '-ac', '1', '-c:a', 'pcm_s16le', wavfile.path];
+    setState(() {
+      _consoleText += "\$ ffmpeg ${ args.join(' ') }\n";
+    });
+    var result = await Process.run('ffmpeg', args);
+    setState(() {
+      _consoleText += result.stderr + result.stdout + '\n';
+    });
+    return wavfile;
+  }
+
+  Future<String> _transcript(File wavfile) async {
+    String whisperPath = path.join(_appTempDir!.path, 'app', 'whispercpp');
+    String modelPath = path.join(_appTempDir!.path, 'app', 'ggml-medium.bin');
+    var args = ['-m', modelPath, '-l', 'ja', '-f', wavfile.path];
+    setState(() {
+      _consoleText += "\$ $whisperPath ${ args.join(' ') }\n";
+    });
+    var result = await Process.run(whisperPath, args);
+    var resultText = result.stderr + result.stdout;
+    setState(() {
+      _consoleText += resultText + '\n';
+    });
+    return resultText;
   }
 }
